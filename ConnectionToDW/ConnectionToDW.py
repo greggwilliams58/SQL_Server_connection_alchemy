d39cf92fc4a83939e17e47071bd90db58a96686d
Greg Williams <greggwilliams58@hotmail.com>
2019-08-23 09:11:18 +0100

    Update ConnectionToDW.py

--- ConnectionToDW/ConnectionToDW.py
+++ ConnectionToDW/ConnectionToDW.py
@@ -1,13 +1,15 @@
 
 import pyodbc
 import sqlalchemy
-import urllib
+import pandas as pd
+from pandas import DataFrame
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 from sqlalchemy import Table
 from sqlalchemy import select
 from sqlalchemy import inspect
 import pprint as pp
+import time
 
 
 def main():
@@ -20,16 +22,46 @@ def main():
 
     print(inspector.get_table_names())
 
-    stmt = 'SELECT * FROM [ORR_DW].[NETL].[factt_206_rollingstock]'
+    start = time.time()
 
-    results = conn.execute(stmt).fetchall()
+    #stmt = 'SELECT * FROM [ORR_DW].[NETL].[factt_206_rollingstock]'
+    stmt = """SELECT TOP (1000) [Lennon_ID]
+      ,[source_item_id]
+      ,[status_id]
+      ,[Date of Settlement]
+      ,[Date key]
+      ,[Quarter]
+      ,[Carrier Profit Centre Code]
+      ,[Carrier Profit Centre Desc]
+      ,[Carrier Subdivision Code]
+      ,[Carrier Subdivision Desc]
+      ,[Service Code]
+      ,[Service Desc]
+      ,[Sector_Franchise Combo]
+      ,[Sector_Franchise]
+      ,[train_operating_company_key]
+      ,[Product Code]
+      ,[Product Level 1 Code]
+      ,[Ordinary/Season Ticket]
+      ,[Ticket Type]
+      ,[Ticket Class]
+      ,[Reduced/Full/Other Ticket]
+      ,[Lennon Adjusted Earnings (Sterling)]
+      ,[Lennon Operating Miles]
+      ,[Lennon Operating KM]
+      ,[Operating Journeys]
+      ,[route_key]
+      ,[ticket_type_key]
+  FROM [ORR_DW].[ORR].[factt_205_lennon_ch1]"""
+ 
 
-    print(f"results are {type(results)}")
-    for line in results:
-        print(f"each line from results is {type(line)}")
-        pp.pprint(line)
+    df = pd.read_sql(stmt,conn)
+    print(df)
 
 
+
+    end = time.time()
+    print(f"the code needed {end-start} seconds")
 
 if __name__ == '__main__':
     main()
